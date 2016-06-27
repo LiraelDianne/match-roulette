a854445abf913799de4f9938e453cf46545258eb
BobSnyder <bob@bobsnyderdesign.com>
2016-06-27 15:00:24 -0700

    added 2nd app, fixed routes

--- apps/login_reg/urls.py
+++ apps/login_reg/urls.py
@@ -2,10 +2,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name = "br_index"),
-    url(r'^login$', views.login, name = "br_login"),
-    url(r'^register$', views.register, name = "br_register"),
-    url(r'^reset$', views.reset, name = "br_reset"),
-    url(r'^home$', views.home, name = "br_home"),
+    url(r'^$', views.index, name = "rl_index"),
+    url(r'^login$', views.login, name = "rl_login"),
+    url(r'^register$', views.register, name = "rl_register"),
+    url(r'^reset$', views.reset, name = "rl_reset"),
+    url(r'^home$', views.home, name = "rl_home"),
+    url(r'^profile/(?P<profile_name>\d+)$', views.profile, name = 'rl_profile')
 ]
 #url(r'^this_app/(?P<id>[0-9]+)/edit$', views.edit, name = 'my_edit'),
+
+#url(r'comments/(?:page-(?P<page_number>\d+)/)?$', comments)
